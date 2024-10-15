3f55a513cf05fbaaec23512fd7e58137d54ecac7
Quinten <quinten.nagel01@gmail.com>
2024-10-15 12:57:44 +0200

    validation added.

--- addressbook.py
+++ addressbook.py
@@ -3,6 +3,24 @@ import sys
 import json
 
 
+def validate(f_name, l_name, emails, phone,):
+    if f_name.isalpha() and l_name.isalpha():
+        if any("@" in email for email in emails):
+            for phone_number in phone:
+                if phone_number.isdigit():
+                    return True
+                else:
+                    return False
+        else:
+            return False
+    else:
+        return False
+
+
+def new_id():
+    return len(read_from_json("contacts.json")) + 1
+
+
 '''
 print all contacts in the following format:
 ======================================
@@ -19,11 +37,12 @@ def display(addressbook: list):
 '''
 return list of contacts sorted by first_name or last_name [if blank then unsorted], direction [ASC (default)/DESC])
 '''
-def list_contacts(...):
+def list_contacts():
+    pass
     # todo: implement this function
     ...
 
-    return addressbook
+    # return addressbook
 
 
 '''
@@ -33,17 +52,29 @@ add new contact:
 - emails = {}
 - phone_numbers = {}
 '''
-def add_contact(...):
-    # todo: implement this function
-    ...
-
+def add_contact():
+    first_name = input("First name: ")
+    last_name = input("Last name: ")
+    email_adresses = list(input("Emails: ").split(","))
+    phone_numbers = list(input("Phone numbers: ").split(","))
+    if validate(first_name, last_name, email_adresses, phone_numbers):
+        id = new_id()
+        new_contact = {"id": id, "first_name": first_name, "last_name":
+            last_name, "emails": email_adresses,"phone_numbers": phone_numbers}
+        print(new_contact)
+        
+        print("Contact added to addressbook")
+        main("contacts.json")
+    else:
+        print("idfk")
+    # print(first_name, last_name, emails, phone_numbers)
 
 '''
 remove contact by ID (integer)
 '''
-def remove_contact(...):
+def remove_contact():
     # todo: implement this function
-    ...
+    pass
 
 
 '''
@@ -81,27 +112,35 @@ def write_to_json(filename, addressbook: list) -> None:
         outfile.write(json_object)
 
 
-'''
-main function:
-# build menu structure as following
-# the input can be case-insensitive (so E and e are valid inputs)
-# [L] List contacts
-# [A] Add contact
-# [R] Remove contact
-# [M] Merge contacts
-# [Q] Quit program
-Don't forget to put the contacts.json file in the same location as this file!
-'''
-
 def main(json_file):
     addressbook = read_from_json(json_file)
-
-    # todo: implement this function.
+    print("""
+Menu
+[L] List contacts
+[A] Add contact
+[R] Remove contact
+[M] Merge contacts
+[Q] Quit program""")
+    while True:
+        option = input("").lower()
+        if option == "l":
+            list_contacts(addressbook)
+            break
+        elif option == "a":
+            add_contact()
+            break
+        elif option == "r":
+            remove_contact()
+            break
+        elif option == "m":
+            merge_contacts()
+            break
+        elif option == "q":
+            quit()
+        else:
+            print("That is not a valid option")
+            continue
     
 
-'''
-calling main function: 
-Do NOT change it.
-'''
 if __name__ == "__main__":
     main('contacts.json')
